eac9b9e745715a0c586e9655e9928f5f228d922d
alexhuynh0530 <88806716+alexhuynh0530@users.noreply.github.com>
2021-10-10 23:53:50 -0500

    add challenge query

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -10,7 +10,7 @@ ORDER BY e.emp_no
 
 -- Use Dictinct with Orderby to remove duplicate rows on retirement_titles
 SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
-INTO unique_titles
+-- INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no ASC, to_date DESC;
 
@@ -28,18 +28,14 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no,
 	e.last_name, 
 	e.birth_date, 
 	de.from_date,
-	de.to_date,
+	t.to_date,
 	t.title
--- INTO retirement_titles
+INTO mentorship_eligibilty
 FROM employees AS e
 JOIN dept_emp AS de
 ON e.emp_no = de.emp_no
 JOIN titles AS t
 ON e.emp_no = t.emp_no
-WHERE (de.to_date = '9999-01-01')
-	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+WHERE (t.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
 
-SELECT *
-FROM titles
-WHERE emp_no = 10095
